19419cbd9c9d74f519264b9a6f3e2a8aeb6b6612
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-05-17 23:16:46 +0200

    🐛 fixed order add and update lager

--- backend/models/order-model.js
+++ backend/models/order-model.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-    orderNumber: {
-        type: Number,
-        required: true},
-    products: {
-        type: [mongoose.Types.ObjectId],
-        ref: "product"},
-    quantity: {
-        type: Number,
-        required: true},
+    products: [{
+        product: {
+            type: mongoose.Types.ObjectId,
+            ref: "product"  // Update the reference name to match the model name
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }],
     user: {
-        type: [mongoose.Types.ObjectId],
-        ref: "user"}
-})
+        type: mongoose.Types.ObjectId,
+        ref: "user"
+    }
+});
 
 
 
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,32 +4,41 @@ const UserModel = require("../models/user-model");
 const ProductModel = require("../models/product-model");
 var router = express.Router();
 
-router.get('/all',async function(request, response) {
-   const orders = await OrderModel.find().populate("products");
-   response.status(200).json(orders);
-  });
-
-router.post("/add", async function(request,response) {
-  const {orderNumber, productId, quantity, userId} = request.body;
-  const user = await UserModel.findOne({_id: userId});
+router.post("/add", async function(request, response) {
+  const { products, user } = request.body;
+  const currentUser = await UserModel.findById(user);
   const orderedProducts = [];
-  for (let i = 0; i < orderedProducts.length; i++) {
-    const product = await ProductModel.findById(orderedProducts[i]);
+
+  let totalQuantity = 0; 
+
+  for (let i = 0; i < products.length; i++) {
+    const product = await ProductModel.findById(products[i].productId);
     if (!product) {
       return response.status(400).send(`Product was not found`);
     }
-    orderedProducts.push(product._id);
+
+    orderedProducts.push({
+      product: product._id,
+      quantity: products[i].quantity
+    });
+
+    
+
+    totalQuantity += products[i].quantity; 
+    product.lager -= products[i].quantity;
+    await product.save();
   }
+
   const order = new OrderModel({
-    orderNumber: orderNumber,
     products: orderedProducts,
-    quantity: quantity,
-    user: user
+    quantity: totalQuantity, 
+    user: currentUser._id
   });
-  
+
   await order.save();
   response.send(order);
 });
+
   
   
 module.exports = router;
